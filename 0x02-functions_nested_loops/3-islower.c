79f2ec7bafec3417eaa6a8bbe8eec11d05f06e8a
Alex Soyoye <alexsoyoye@live.com>
2022-12-13 18:50:48 -0800

    commit 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,12 +4,12 @@
 
 /**
  * _islower - Checks if a character is lowercase.
- * c: The character to be checked.
+ * @c: The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
 
-int _islower(int c) 
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
